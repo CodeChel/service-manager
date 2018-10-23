76ee1598b9924ea7870b610362705eb3fa7bced6
ChelCod <vladislavbezverhnii@gmail.com>
2018-10-23 07:53:19 +0300

    js pure slider

--- js/main.js
+++ js/main.js
@@ -23,11 +23,82 @@ var count;
 
 var total_comment =  document.querySelectorAll('.comment');
 
-btn_down.addEventListener('click', function () {
-	for (count = 0; count < total_comment.length; count++) {
-				if (total_comment[count].style.order) {
-			total_comment[count].style.order = count + 3;
-			return;
-		}
+
+function test () {
+
+														if( getComputedStyle(total_comment[0]).top == '0px' ){
+				 	 											btn_up.style.display = 'none';
+				 	 										}
+				 	 										else {
+				 	 											btn_up.style.display = 'block';
+				 	 										}
+				 	 										if( getComputedStyle(total_comment[total_comment.length-1]).top == '240px' ){
+				 	 											btn_down.style.display = 'none';
+				 	 										}
+				 	 										else {
+				 	 											btn_down.style.display = 'block';
+				 	 										}
 }
-})
+									
+
+
+
+
+
+btn_up.addEventListener('click', function () {
+		var start = Date.now();	
+				 	 
+				 	 var timer = setInterval(function(){
+				 	 																																										// animation
+				 	 								for (count = 0; count < total_comment.length; count++){
+				 	 									var computedStyle = getComputedStyle(total_comment[count]);
+				 	 						
+				 	 									var timePassed = Date.now() - start;
+				 	 									total_comment[count].style.top = (parseInt(computedStyle.top) + 24  ) + 'px' ;
+				 	 									if (timePassed >= 200) {
+				 	 										test();	
+				 	 										clearInterval(timer);
+				 	 									}
+
+				 	 				}
+				 	 		
+				 },20);
+						
+		}
+		)
+
+
+btn_down.addEventListener('click', function () {
+
+		var start = Date.now();	
+				 	 
+				 	 var timer = setInterval(function(){
+				 	 																																										// animation
+				 	 								for (count = 0; count < total_comment.length; count++){
+				 	 									var computedStyle = getComputedStyle(total_comment[count]);
+				 	 										
+				 	 									var timePassed = Date.now() - start;
+				 	 									total_comment[count].style.top = (parseInt(computedStyle.top) - 24  ) + 'px' ;
+				 	 									if (timePassed >= 200) {
+				 	 										test();	
+				 	 										clearInterval(timer);
+				 	 									}
+				 	 									
+
+				 	 				}
+				 	 		
+				 },20);
+				 	 									
+				
+		}
+		)
+		
+				 	
+				 		
+				 	
+				 		
+				 		
+
+				 		
+
+				  
